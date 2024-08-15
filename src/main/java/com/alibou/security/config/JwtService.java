c8aa4b2a13e2961e923a095c87091c4e8727bf5a
hariprasath36 <hariprasa36@gmail.com>
2024-08-15 20:04:39 +0530

    bug at 1.08.31

---
+++ src/main/java/com/alibou/security/config/JwtService.java
@@ -0,0 +1,57 @@
+package com.alibou.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.security.Signature;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY = "QHHpWcp9M7MXZcHWbice0UFQsyqzPdgGrCOJemG63WmQRbcKDegnWcp40m4J4Manvd7xFPTJrC4cqgkj5iOGlWwsD2d4gwRBNk17O6/kBZwmby2gWHUn12IR+D+epjkBonGhV9M2NPNB7RxyHUt82IF2rhr/9+mm+qZK2EZ0m2frj6Mm3WLcn/jkIVORYXiP/pWVCz6hiZEFXus6Vdfz1SrjDmBdE79pJgfzwco0QidxDvdIVigvK4fNVpKmcp3O2CKzXrEBNF7nwG/5ncwWbk5gK30HD/2XO/DoWGV95ETz409pNJG5mtcc4mvNFWlVTqV7dWz13zShP0MWA7v3OA2bawiS0A75+eh3KJxUnIo=";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 *24)))
+
+
+
+    }
+
+    private Claims extractAllClaims(String token) {  // Corrected method name
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
